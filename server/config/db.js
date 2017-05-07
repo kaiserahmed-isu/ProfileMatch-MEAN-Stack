8db5b8b9de9f8bf6ce25b3c60cfa74dfad700d84
Kaiser Ahmed <kaiserahmed.isu@gmail.com>
2017-05-07 13:40:52 -0500

    fixed for heroku 1

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
     var mongoose = require('mongoose');
-    var connection = mongoose.connect('mongodb://kaiser:poiPOI098)(*@ds133321.mlab.com:33321/client_db');
+//    var connection = mongoose.connect('mongodb://kaiser:poiPOI098)(*@ds133321.mlab.com:33321/client_db');
+    var connection = mongoose.connect('mongodb://kaiser:poiPOI098)(*@ds133271.mlab.com:33271/client_db_2');
      
     module.exports = connection;
